acba1324f080ccf8ba3230bea67bdcb828abfe66
Knightly <knightly.cavalier@gmail.com>
2020-12-27 09:42:36 -1000

    Remove vcpkg scripts

--- PostClean.ps1
+++
@@ -1,6 +0,0 @@
-param([string]$VCPkgRoot);
-Set-Location $VCPkgRoot
-if (!(Test-Path ".\vcpkg.exe")) {
-    & ".\bootstrap-vcpkg.bat"
-}
-& ".\vcpkg.exe" remove --triplet x86-windows-static yaml-cpp sleepy-discord
--- PreBuild.ps1
+++
@@ -1,6 +0,0 @@
-param([string]$VCPkgRoot);
-Set-Location $VCPkgRoot
-if (!(Test-Path ".\vcpkg.exe")) {
-    & ".\bootstrap-vcpkg.bat"
-}
-& ".\vcpkg.exe" install yaml-cpp:x86-windows-static sleepy-discord[websocketpp]:x86-windows-static
